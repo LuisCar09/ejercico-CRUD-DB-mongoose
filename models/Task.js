4a824b6393d03790fdc096896c52c8ca1f2bda6f
Luis Carlos <luiscbravo94@gmail.com>
2024-09-21 18:39:50 +0100

    Project started, task schema done

---
+++ models/Task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+const TaskSchema = new mongoose.Schema({
+    task: String,
+    completed : Boolean
+},{timestamps:true})
+
+const Task = mongoose.model('Task',TaskSchema);
+
+module.exports = Task
